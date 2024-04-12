92ea374e81548820665a1d601fb27e0164cdfa89
vikash.chauhan <vikash.chauhan@tothenew.com>
2024-04-12 13:49:08 +0530

    work on node arguments

--- Notes-app/app.js
+++ Notes-app/app.js
@@ -1,11 +1,73 @@
 // const fs = require('fs');
-const validator = require('validator')
+// const validator = require('validator')
 const notes = require('./notes');
-const chalk = require('chalk');
-const log = console.log
+// const chalk = require('chalk');
 
-console.log(notes());
-console.log(validator.isEmail('vc90841gmail.com'));
-log(chalk.red('hello worlds'));
+const yargs = require('yargs')
+// const log = console.log
+
+// console.log(notes());
+// console.log(validator.isEmail('vc90841gmail.com'));
+// log(chalk.red('hello worlds'));
 // fs.writeFileSync('note.txt','node js first file /n');
 // fs.appendFileSync('note.txt','append new text')
+
+// const parseArgument = process.argv[2];
+// console.log(parseArgument);
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe:'this is a title node',
+            demandOption:true,
+            type:String
+        },
+        body:{
+            describe:'this is a body',
+            demandOption:true,
+            type:String
+        }
+    },
+
+    handler: function (argv) {
+        // console.log('Adding a new note!',argv.title);
+        // console.log('Adding a new body!',argv.body);
+        notes.addNotes(argv.title,argv.body);
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note',
+    handler: function (argv) {
+        notes.removeNotes(argv.title);
+       
+    }
+})
+
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+       notes.listNotes();
+    }
+})
+
+
+yargs.command({
+    command: 'read',
+    describe: 'Read note',
+    handler: function (argv) {
+       notes.readCommand(argv.title);
+    }
+})
+
+yargs.parse();
+
+
--- Notes-app/notes.js
+++ Notes-app/notes.js
@@ -1,4 +1,78 @@
+const fs = require('fs')
+const chalk = require('chalk');
 const  getNotes = () => {
     return 'yourNotes'
 }
-module.exports = getNotes
+
+const addNotes = (title,body) => {
+    const notes = loadNotes();
+    const duplicateNotes = notes.find((note) => note.title === title);
+    if(duplicateNotes) {
+        console.log('load is exist already')
+    }
+    else {
+        notes.push({
+            title:title,
+            body:body
+        })
+        saveNotes(notes);
+    }
+ 
+
+}
+
+const loadNotes = () => {
+    try{
+     const file =fs.readFileSync('notes.json')
+     const databuffer = file.toString();
+     return (JSON.parse(databuffer));
+
+    } catch {
+        return []
+    }
+
+}
+
+const removeNotes =(title) => {
+    const notes = loadNotes();
+    const removeNotesData = notes.filter((note) => note.title !== title)
+    if(notes.length === removeNotesData.length) {
+        console.log(chalk.red('no node remove'))
+    } else {
+        console.log(chalk.green('node remove'))
+    }
+    saveNotes(removeNotesData);
+}
+
+const saveNotes = (notes) => {
+ const stringfyData = JSON.stringify(notes);
+ fs.writeFileSync('notes.json',stringfyData);
+
+}
+const listNotes = () => {
+    const notes = loadNotes();
+    notes.forEach(element => {
+        console.log(chalk.green(element.title));
+    });
+   
+   }
+const readCommand = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.green(note.title))
+        console.log(chalk.yellow(note.body))
+    } else {
+        console.log(chalk.red.inverse('not found'))
+    }
+
+}
+module.exports = {
+    addNotes:addNotes,
+    loadNotes:loadNotes,
+    saveNotes:saveNotes,
+    removeNotes:removeNotes,
+    listNotes:listNotes,
+    readCommand:readCommand
+}

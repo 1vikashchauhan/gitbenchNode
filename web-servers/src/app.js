78a82bcf241885f468c9728a57f92290a3b2d5f2
vikash.chauhan <vikash.chauhan@tothenew.com>
2024-04-22 06:51:38 +0530

    section-8- work on input file and css

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -1,6 +1,8 @@
 const express =require('express');
 const path = require('path')
 const hbs = require('hbs');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 
 const app = express();
@@ -18,21 +20,53 @@ app.set('view engine', 'hbs');
 app.set('views',viewpaths);
 hbs.registerPartials(partialpaths);
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        forecast: 'raining',
-        location: 'gurugram'
+        products: []
     })
 })
 app.get('', (req, res) => {
     res.render('index', {
         name:"vikash",
-        title:"error"
+        title:"Whether app"
     });
 })
 app.get('/help', (req, res) => {
     res.render('help', {
         help:"help me please",
-        title:"error"
+        title:"Help"
     });
 })
 app.get('*', (req, res) => {
---
+++ web-servers/src/utils/geocode.js
@@ -0,0 +1,20 @@
+const Geocodio = require('geocodio-library-node');
+const geocoder = new Geocodio('67f1a2e46ea336a676f41f6b1b761eee6eb6433');
+const geocode = (address,callback) => {
+  
+    geocoder
+      .geocode(address)
+      .then(response => {
+
+        console.log('response',response);
+        if(response) callback(undefined,{
+            latitude:response.results[0].location.lat,
+            longitude:response.results[0].location.lng
+
+        })
+      }).catch(error => {
+        if(error) callback('unable to find location',undefined)
+      });
+}
+
+module.exports = geocode;
